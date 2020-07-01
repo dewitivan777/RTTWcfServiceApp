412043f16d32c26c698ac4e7de9ca8165b8c0b23
Ivan De wit <deWit.Ivan777@gmail.com>
2020-07-01 11:30:03 +0200

    Add project files.

---
+++ WcfServiceApp/IService1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        //Select statements
+        [OperationContract]
+        UserData Get(UserDetails userInfo);
+
+        [OperationContract]
+        IEnumerable<UserDetails> GetAll(UserDetails userInfo);
+
+        [OperationContract]
+        UserAddress GetAddress(UserAddressDetails addressInfo);
+
+        [OperationContract]
+        IEnumerable<UserAddressDetails> GetUserAddress(UserAddressDetails addressInfo);
+
+        [OperationContract]
+        ExportData GetAllExportDetails();
+
+        //Insert statements
+        [OperationContract]
+        bool InsertUserDetails(UserDetails userInfo);
+        [OperationContract]
+        bool InsertUserAddress(UserAddressDetails addressInfo);
+
+        //update statements
+        [OperationContract]
+        bool Update(UserDetails userInfo);
+        bool UpdateAddress(UserAddressDetails addressInfo);
+
+        //Delete statements
+        [OperationContract]
+        bool Delete(string Id);
+        bool DeleteAddress(string Id);
+    }
+
+    [DataContract]
+    public class ExportData
+    {
+        public ExportData()
+        {
+            this.ExportTable = new DataTable("ExportTable");
+        }
+
+        [DataMember]
+        public DataTable ExportTable { get; set; }
+    }
+
+
+
+    [DataContract]
+    public class UserData
+    {
+        public UserData()
+        {
+            this.UsersTable = new DataTable("UserTable");
+        }
+
+        [DataMember]
+        public DataTable UsersTable { get; set; }
+    }
+
+    [DataContract]
+    public class UserAddress
+    {
+        public UserAddress()
+        {
+            this.AddressTable = new DataTable("UserAddress");
+        }
+
+        [DataMember]
+        public DataTable AddressTable { get; set; }
+    }
+
+    public class UserAddressDetails
+
+    {
+        string id = string.Empty;
+        string userid = string.Empty;
+        string address = string.Empty;
+        string address_type = string.Empty;
+        string city = string.Empty;
+        string province = string.Empty;
+        string zipcode_postalcode = string.Empty;
+
+        [DataMember]
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string UserId
+        {
+            get { return userid; }
+            set { userid = value; }
+        }
+
+        [DataMember]
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        [DataMember]
+        public string AddressType
+        {
+            get { return address_type; }
+            set { address_type = value; }
+        }
+
+        [DataMember]
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        [DataMember]
+        public string Province
+        {
+            get { return province; }
+            set { province = value; }
+        }
+
+        [DataMember]
+        public string Zipcode_PostalCode
+        {
+            get { return zipcode_postalcode; }
+            set { zipcode_postalcode = value; }
+        }
+    }
+
+    public class UserDetails
+    {
+        string userid = string.Empty;
+        string firstname = string.Empty;
+        string surname = string.Empty;
+        string dob = string.Empty;
+        string gender = string.Empty;
+        string mobile = string.Empty;
+        string email = string.Empty;
+        string workmobile = string.Empty;
+
+        [DataMember]
+        public string Userid
+        {
+            get { return userid; }
+            set { userid = value; }
+        }
+
+        [DataMember]
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        [DataMember]
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        [DataMember]
+        public string DOB
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        [DataMember]
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        [DataMember]
+        public string Mobile
+        {
+            get { return mobile; }
+            set { mobile = value; }
+        }
+
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        [DataMember]
+        public string Workmobile
+        {
+            get { return workmobile; }
+            set { workmobile = value; }
+        }
+    }
+}
---
+++ WcfServiceApp/Service1.svc.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Configuration;
+
+namespace WcfServiceApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        SqlConnection con =
+            new SqlConnection(WebConfigurationManager.ConnectionStrings["DBConstring"].ConnectionString);
+
+        public bool InsertUserDetails(UserDetails userInfo)
+        {
+            bool success = false;
+
+            con.Open();
+            //Check if email is already in use
+            SqlCommand validate = new SqlCommand("select count(*) from UserTable where Email = @email", con);
+            validate.Parameters.AddWithValue("@email", userInfo.Email);
+            int count = Convert.ToInt32(validate.ExecuteScalar());
+
+            if (count == 0)
+            {
+                SqlCommand cmd =
+                    new SqlCommand(
+                        "insert into UserTable(UserId,Firstname,Surname,DateOfBirth,Gender,Mobile,Email,WorkMobile) values(@userid,@firstname,@surname,@dob,@gender,@mobile,@email,@work_mobile)",
+                        con);
+
+                cmd.Parameters.AddWithValue("@userid",  Guid.NewGuid().ToString("N"));
+                cmd.Parameters.AddWithValue("@firstname", userInfo.Firstname);
+                cmd.Parameters.AddWithValue("@surname", userInfo.Surname);
+                cmd.Parameters.AddWithValue("@dob", userInfo.DOB);
+                cmd.Parameters.AddWithValue("@gender", userInfo.Gender);
+                cmd.Parameters.AddWithValue("@mobile", userInfo.Mobile);
+                cmd.Parameters.AddWithValue("@email", userInfo.Email);
+                cmd.Parameters.AddWithValue("@work_mobile", userInfo.Workmobile);
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result == 1)
+                {
+                    success = true;
+                }
+            }
+            else
+            {
+                con.Close();
+                return success;
+            }
+
+            con.Close();
+            return success;
+        }
+
+        public bool InsertUserAddress(UserAddressDetails userInfo)
+        {
+            bool success=false;
+
+            con.Open();
+
+            SqlCommand cmd =
+                new SqlCommand(
+                    "insert into AddressTable(User_ID,Address_type,Address,City,Province,[Zip/Postal_Code]) values(@userid,@address_type,@address,@city,@province,@zip)",
+                    con);
+
+            cmd.Parameters.AddWithValue("@id", Guid.NewGuid().ToString("N"));
+            cmd.Parameters.AddWithValue("@userid", userInfo.UserId);
+            cmd.Parameters.AddWithValue("@address_type", userInfo.AddressType);
+            cmd.Parameters.AddWithValue("@address", userInfo.Address);
+            cmd.Parameters.AddWithValue("@city", userInfo.City);
+            cmd.Parameters.AddWithValue("@province", userInfo.Province);
+            cmd.Parameters.AddWithValue("@zip", userInfo.Zipcode_PostalCode);
+
+            int result = cmd.ExecuteNonQuery();
+
+            if (result == 1)
+            {
+                success = true;
+            }
+            
+            con.Close();
+
+            return success;
+        }
+
+        //Get All Users
+        public IEnumerable<UserDetails> GetAll(UserDetails userInfo)
+        {
+            var users = new List<UserDetails>();
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("Select * from UserTable", con))
+                {
+                    cmd.Parameters.AddWithValue("@userId", userInfo.Userid);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            try
+                            {
+                                var user = new UserDetails();
+
+                                user.Userid = reader.IsDBNull(0) ? null : reader.GetString(0);
+                                user.Firstname = reader.IsDBNull(0) ? null : reader.GetString(1);
+                                user.Surname = reader.IsDBNull(0) ? null : reader.GetString(2);
+                                user.DOB = reader.IsDBNull(0) ? null : reader.GetString(3);
+                                user.Gender = reader.IsDBNull(0) ? null : reader.GetString(4);
+                                user.Mobile = reader.IsDBNull(0) ? null : reader.GetString(5);
+                                user.Email = reader.IsDBNull(0) ? null : reader.GetString(6);
+                                user.Workmobile = reader.IsDBNull(0) ? null : reader.GetString(7);
+
+                                users.Add(user);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Failed to read line: " + reader.ToString() + " Error: " + ex);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to make connection: " + ex);
+            }
+
+            return users;
+        }
+
+        //Get User
+        public UserData Get(UserDetails userInfo)
+        {
+            using (SqlCommand cmd = new SqlCommand("Select * from UserTable where UserId = @userId", con))
+            {
+                cmd.Parameters.AddWithValue("@userId", userInfo.Userid);
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        UserData users = new UserData();
+                        sda.Fill(users.UsersTable);
+                        return users;
+                    }
+                }
+            }
+        }
+
+        //Get Address
+        public UserAddress GetAddress(UserAddressDetails userInfo)
+        {
+
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "Select Address_type,Address,City,[Zip/Postal_Code],Province from UserAddressTable where Id = @id",
+                    con))
+            {
+                cmd.Parameters.AddWithValue("@id", userInfo.Id);
+
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        var addresses = new UserAddress();
+                        sda.Fill(addresses.AddressTable);
+
+                        return addresses;
+                    }
+                }
+            }
+        }
+
+        //Get All User's Addresses
+        public IEnumerable<UserAddressDetails> GetUserAddress(UserAddressDetails userInfo)
+        {
+            var addresses = new List<UserAddressDetails>();
+
+            try
+            {
+                using (SqlCommand cmd =
+                    new SqlCommand(
+                        "Select Address_type,Address,City,[Zip/Postal_Code],Province from UserAddressTable where User_ID = @userid",
+                        con))
+                {
+                    cmd.Parameters.AddWithValue("@userid", userInfo.UserId);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            try
+                            {
+                                var address = new UserAddressDetails();
+                                address.Id = reader.IsDBNull(0) ? null : reader.GetString(0);
+                                address.UserId = reader.IsDBNull(0) ? null : reader.GetString(1);
+                                address.Address = reader.IsDBNull(0) ? null : reader.GetString(2);
+                                address.AddressType = reader.IsDBNull(0) ? null : reader.GetString(3);
+                                address.City = reader.IsDBNull(0) ? null : reader.GetString(4);
+                                address.Province = reader.IsDBNull(0) ? null : reader.GetString(5);
+                                address.Zipcode_PostalCode = reader.IsDBNull(0) ? null : reader.GetString(6);
+                                addresses.Add(address);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Failed to read line: " + reader.ToString() + " Error: " + ex);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to make connection: " + ex);
+            }
+
+            return addresses;
+        }
+
+        //Update User
+        public bool Update(UserDetails userInfo)
+        {
+            bool success = false;
+            using (SqlCommand cmd = new SqlCommand("UPDATE UserTable SET Firstname = @name, Surname = @surname,Date_of_birth = @dob,Gender = @gender,Mobile = @mobile,Email = @email,Work_mobile =@work WHERE id = @userid", con))
+            {
+                cmd.Parameters.AddWithValue("@name", userInfo.Firstname);
+                cmd.Parameters.AddWithValue("@surname", userInfo.Surname);
+                cmd.Parameters.AddWithValue("@dob", userInfo.DOB);
+                cmd.Parameters.AddWithValue("@gender", userInfo.Gender);
+                cmd.Parameters.AddWithValue("@mobile", userInfo.Mobile);
+                cmd.Parameters.AddWithValue("@email", userInfo.Email);
+                cmd.Parameters.AddWithValue("@work", userInfo.Workmobile);
+                cmd.Parameters.AddWithValue("@userid", userInfo.Userid);
+
+                cmd.Connection = con;
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+
+                if (result == 1)
+                {
+                    success = true;
+                }
+
+                con.Close();
+                return success;
+            }
+        }
+
+        //Update Address 
+        public bool UpdateAddress(UserAddressDetails userInfo)
+        {
+            bool success = false;
+            using (SqlCommand cmd = new SqlCommand("UPDATE UserAddressTable SET Firstname = @name, Surname = @surname,Date_of_birth = @dob,Gender = @gender,Mobile = @mobile,Email = @email,Work_mobile =@work WHERE id = @userid", con))
+            {
+                cmd.Parameters.AddWithValue("@userid", userInfo.UserId);
+
+                cmd.Connection = con;
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+
+                if (result == 1)
+                {
+                    success = true;
+                }
+
+                con.Close();
+                return success;
+            }
+        }
+
+        public bool Delete(string Id)
+        {
+            bool success = false;
+            using (SqlCommand cmd = new SqlCommand("Delete From UserAddressTable Where UserId = @userid", con))
+            {
+                cmd.Parameters.AddWithValue("@userid", Id);
+
+                cmd.Connection = con;
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+
+                if (result == 1)
+                {
+                    success = true;
+                }
+
+                con.Close();
+                return success;
+            }
+        }
+
+        public bool DeleteAddress(string Id)
+        {
+            bool success = false;
+            using (SqlCommand cmd = new SqlCommand("Delete From UserAddressTable Where Id = @id", con))
+            {
+                cmd.Parameters.AddWithValue("@id", Id);
+
+                cmd.Connection = con;
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                
+                if (result == 1)
+                {
+                    success = true;
+                }
+
+                con.Close();
+                return success;
+            }
+        }
+
+        public ExportData GetAllExportDetails()
+        {
+
+            using (SqlCommand cmd = new SqlCommand("select usrT.Firstname, usrT.Surname,usrT.Gender,usrT.Mobile,usrT.Work_mobile,usrT.Email ,addrT.Address_type,addrT.Address,addrT.City,addrT.Province,addrT.[Zip/Postal_Code] " +
+                "from UserTable usrT left join AddressTable addrT on addrT.User_ID = usrT.id; ", con))
+            {
+
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        ExportData exportDt = new ExportData();
+                        sda.Fill(exportDt.ExportTable);
+                        con.Close();
+                        return exportDt;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
