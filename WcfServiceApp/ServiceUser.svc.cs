7a81edb044a845d2604bed3c562eb27834b418eb
Ivan De wit <deWit.Ivan777@gmail.com>
2020-07-01 11:56:02 +0200

    Update Service Name

--- WcfServiceApp/ServiceUser.svc.cs
+++ WcfServiceApp/ServiceUser.svc.cs
@@ -8,7 +8,7 @@ namespace WcfServiceApp
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class ServiceUser : IServiceUser
     {
         SqlConnection con =
             new SqlConnection(WebConfigurationManager.ConnectionStrings["DBConstring"].ConnectionString);
@@ -69,6 +69,7 @@ namespace WcfServiceApp
         public bool InsertUserAddress(UserAddressDetails userInfo)
         {
             bool success = false;
+
             try
             {
                 con.Open();
